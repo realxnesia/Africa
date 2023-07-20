e9ea1fa13f2d1276cde88a2058226e561b3e9f78
dhika <dhika.aditya@kpvendor.id>
2023-07-20 08:42:29 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by realxnesia on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

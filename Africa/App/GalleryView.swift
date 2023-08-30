d3b3030db1343fe32089ff762c1f6148fe622e1e
dhika <dhika.aditya@kpvendor.id>
2023-08-30 13:38:28 +0700

    Create Motion Effect view

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct GalleryView: View {
-    var body: some View {
-        Text("Gallery")
-    }
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      Text("Gallery")
+    } //: SCROLL
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(MotionAnimationView())
+  }
 }
 
 struct GalleryView_Previews: PreviewProvider {
-    static var previews: some View {
-        GalleryView()
-    }
+  static var previews: some View {
+    GalleryView()
+  }
 }

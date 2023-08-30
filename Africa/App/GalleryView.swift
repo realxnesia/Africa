c24a1718491afe3c9cad71032e6b1837c249344a
dhika <dhika.aditya@kpvendor.id>
2023-08-30 14:34:37 +0700

    Implement grid layout for gallery

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,12 +8,87 @@
 import SwiftUI
 
 struct GalleryView: View {
+  let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
+  
+  @State private var selectedAnimal: String = "lion"
+  
+  // MARK: - SIMPLE GRID DEFINITION
+  /*
+  let gridLayout: [GridItem] = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+  ]
+  */
+  
+  // MARK: - EFFICIENT GRID DEFINITION
+  /*
+  let gridLayout: [GridItem] = Array(
+    repeating: GridItem(.flexible()),
+    count: 3
+  )
+  */
+  
+  // MARK: - DYNAMIC GRID LAYOUT
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridColumn: Double = 3.0
+  
+  
   var body: some View {
     ScrollView(.vertical, showsIndicators: false) {
-      Text("Gallery")
+      VStack {
+        // MARK: - SELECTED IMAGE
+        Image(selectedAnimal)
+          .resizable()
+          .scaledToFit()
+          .clipShape(Circle())
+          .overlay(
+            Circle().stroke(Color.white, lineWidth: 1)
+          )
+        
+        // MARK: - SLIDER
+        Slider(value: $gridColumn, in: 2...4, step: 1)
+          .padding(.horizontal)
+          .onChange(of: gridColumn, perform: { value in
+            gridSwitch()
+          })
+        
+        // MARK: - GRID
+        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+          ForEach(animals) { animal in
+            Image(animal.image)
+              .resizable()
+              .scaledToFit()
+              .clipShape(Circle())
+              .overlay(
+                Circle().stroke(
+                  Color.white, lineWidth: 1
+                )
+              )
+              .onTapGesture {
+                selectedAnimal = animal.image
+                haptics.impactOccurred()
+              }
+          } //: LOOP
+        } //: GRID
+        .animation(.easeIn)
+        .onAppear {
+          gridSwitch()
+        }
+      } //: VSTACK
+      .padding(.horizontal, 10)
+      .padding(.vertical, 50)
     } //: SCROLL
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(MotionAnimationView())
+  }
+  
+  private func gridSwitch() {
+    gridLayout = Array(
+      repeating: .init(.flexible()),
+      count: Int(gridColumn)
+    )
   }
 }
 
